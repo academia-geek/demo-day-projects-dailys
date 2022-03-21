7c9416e5a24ce156da576cf966cfca069a9c929e
Paola Barreto <paola.valentina080670@gmail.com>
2022-03-20 22:25:13 -0500

    Inicio de sesion no responsive

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,11 @@
+import Login from "../components/Login";
 
-import Footer from '../components/Welcome.js/Footer';
-import WelcomePage from '../components/WelcomePage';
+
 
 function App() {
   return (
     <div className="App">
-   <WelcomePage/>
+   <Login/>
   
     </div>
   );
