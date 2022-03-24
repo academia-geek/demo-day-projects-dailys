ed786fb6255fe6524194bd82cd0ffb8335a839f7
jefferson garcia <jdgarcia988@misena.edu.co>
2022-03-24 16:27:56 -0500

    Menu terminado

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -1,45 +1,22 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Image } from 'react-bootstrap'
-import { AgendaComponent } from './AgendaComponent';
-import { TareaComponent } from './TareaComponent';
+import SwipeableTemporaryDrawer from './SwipeableTemporaryDrawer'
 
 export const HomeComponent = () => {
 
-    const [tarea, setTarea] = useState(true);
-
-    const [agenda, setAgenda] = useState(false);
-
-    const handleTarea = () => {
-        setTarea(true)
-        setAgenda(false)
-    }
-    const handleAgenda = () => {
-        setTarea(false);
-        setAgenda(true)
-    }
     return (
         <div>
             <div className="containerHome">
-                <div className="logos"></div>
-                <div className="nav-sg d1" onClick={handleTarea}>
+                <div className="logos">
+                    <SwipeableTemporaryDrawer />
+                </div>
+                <div className="nav-sg d1">
                     <Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903768/cuaderno_2_szskdd_wirxho.png" alt='Tareas'></Image>
                     <h1 className="title" >Tareas</h1>
-                </div>
-                <div className="nav-sg d2" onClick={handleAgenda}>
-                    <Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647905480/contactos_3_cu60sy.png" alt='agenda'></Image>
-                    <h1 className="title" > Agenda</h1>
                 </div>
                 <div className="nav-sg d3">
                     <Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903768/calendario_ia6vj9_sebpfk.png" alt='calendario'></Image>
                     <h1 className="title">Calendario</h1>
-                </div>
-                <div className="nav-sg d4">
-                    <Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903768/barra-grafica_yqegb2_g6aawj.png" alt='estadistica'></Image>
-                    <h1 className="title">Estadistica</h1>
-                </div>
-                <div className="nav-sg d5">
-                    <Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903757/lista-de-tareas_zk7yif.png" alt='encuesta'></Image>
-                    <h1 className="title">Encuesta</h1>
                 </div>
                 <div className="nav-sg d6">
                     <Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903756/perfil-del-usuario_imntjy.png" alt='perfil'></Image>
@@ -48,10 +25,10 @@ export const HomeComponent = () => {
             </div>
             <div className="navMobile">
                 <div className="nav-sd">
-                    <Image className="imgNav" src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903756/lista_1_dexdcr.png" alt='Menu' onClick={handleTarea}></Image>
+                    <SwipeableTemporaryDrawer />
                 </div>
                 <div className="nav-sd">
-                    <Image className="imgNav" src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903757/lista-de-tareas_zk7yif.png" alt='Tareas' onClick={handleAgenda}></Image>
+                    <Image className="imgNav" src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903757/lista-de-tareas_zk7yif.png" alt='Tareas'></Image>
                 </div>
                 <div className="nav-sd">
                     <Image className="imgNav" src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903756/calendario_3_gcsfru.png" alt='calendario'></Image>
@@ -61,12 +38,7 @@ export const HomeComponent = () => {
                 </div>
             </div>
             <div className="content">
-                {
-                    tarea === true ? <TareaComponent /> : ''
-                }
-                {
-                    agenda === true ? <AgendaComponent /> : ''
-                }
+
             </div>
         </div>
     )
---
+++ src/components/Home/SwipeableTemporaryDrawer.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import { Image, Nav } from 'react-bootstrap';
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+export default function SwipeableTemporaryDrawer() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    left: false
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+      </List>
+    </div>
+  );
+
+  return (
+    <div className="drawe">
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{<Image src="https://res.cloudinary.com/dsnsjqasu/image/upload/v1647903756/lista_1_dexdcr.png" alt='perfil'></Image>}</Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
