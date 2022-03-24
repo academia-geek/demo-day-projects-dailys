969e52dee5ef30ba81242f0e2ed526e3b0c0c256
jefferson garcia <jdgarcia988@misena.edu.co>
2022-03-24 18:21:48 -0500

    merge a master

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
 import "./styles/landing/stylesLanding.css"
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
@@ -9,11 +8,10 @@ import "../src/styles/registro/stylesRegistro.css"
 import "../src/styles/calendar/stylesCalendar.css"
 import './styles/Home/homeStyle.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <Provider store={store}>
-    <AppRouter/>
+    <AppRouter />
   </Provider>,
   document.getElementById('root')
 );
-
-
