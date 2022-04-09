21aed88c23ed97f710b9dade733234830f80123a
jefferson garcia <jdgarcia988@misena.edu.co>
2022-04-08 20:23:22 -0500

    arreglos

--- src/components/estadistica/Estadistica.js
+++ src/components/estadistica/Estadistica.js
@@ -7,7 +7,6 @@ import { Rangoe } from './Rangoe';
 import { Rendimiento } from './Rendimiento';
 import { TabTask } from './TabTask';
 import "../../styles/estadistica/est.css"
-import { Link } from 'react-router-dom';
 import { HomeComponent } from '../Navbar/Home/HomeComponent'
 
 export const Estadistica = () => {
@@ -22,7 +21,7 @@ export const Estadistica = () => {
         <div>
 
             <HomeComponent />
-            
+
             <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li className="nav-item" role="presentation">
                     <button className="nav-link active botones"
@@ -34,7 +33,6 @@ export const Estadistica = () => {
                         aria-controls="pills-home"
                         aria-selected="true">Tareas</button>
                 </li>
-
                 <li class="nav-item" role="presentation">
                     <button className="nav-link botones"
                         id="pills-emociones-tab"
@@ -48,25 +46,15 @@ export const Estadistica = () => {
             </ul>
             <div className="tab-content mx-auto" id="pills-tabContent">
                 <div className="tab-pane fade show active" id="pills-total" role="tabpanel" aria-labelledby="pills-total-tab">
-
                     <Listar className="d-inline-block" />
-
                     <TabTask className="d-inline-block w-25" />
                     <Rendimiento />
-
                 </div>
-
                 <div className="tab-pane fade" id="pills-emociones" role="tabpanel" aria-labelledby="pills-emociones-tab">
-
                     <Listaremociones />
                     <Rangoe />
-
                 </div>
             </div>
-
-
-
-
         </div>
     )
 }
